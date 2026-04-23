ee32a539cb2bc88ff014f2a3fb00fa05a3000b6e
Taras Torchkov <tt4506r@gre.ac.uk>
2026-04-23 13:47:56 +0100

    готово к проверке

--- fit_life.py
+++ fit_life.py
@@ -10,13 +10,12 @@ def bmi():
     user_weight = float(input("Пожалуйста, введите ваш вес в килограммах: "))
     user_height = float(input("Введите ваш рост в метрах, используя точку: "))
     bmi = round(user_weight / (user_height ** 2), 1)
-    water_ml = user_weight * WATER_PER_KG
-    water_l = water_ml / ML_IN_L
+    water_l = (user_weight * WATER_PER_KG) / ML_IN_L
 
     print(f'Отчет для пользователя {user_name} ({user_age}).\n'
           f'Твой Индекс Массы Тела: {bmi}\n'
           f'Рекомендуемая норма воды: {water_l} л')
 
 
-bmi()
-print('Расчет окончен\nБудьте здоровы!')
+if __name__ == "__main__":
+    bmi()
